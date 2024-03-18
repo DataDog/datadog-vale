86291bb373f25b9da5e162d65298129a1ba113d1
Brett Blue <brett.blue@datadoghq.com>
2024-03-18 11:54:33 -0600

    Rename file. Add workflow.

--- scripts/generate-overview.py
+++ scripts/generate-overview.py
@@ -6,32 +6,35 @@ def generate_rule_markdown(rule_data):
     message = rule_data['message']
     markdown = f"### {message}\n\n"
     markdown += f"**Level:** *{rule_data['level']}*\n\n"
+
     if 'link' in rule_data:
         markdown += f"[Link]({rule_data['link']})\n\n"
-    if 'tokens' in rule_data:
-        markdown += "**Tokens:**\n"
-        for token in rule_data['tokens']:
-            markdown += f"- `{token}`\n"
-    if 'exceptions' in rule_data:
-        markdown += "\n**Exceptions:**\n"
-        for exception in rule_data['exceptions']:
-            markdown += f"- `{exception}`\n"
+
+    for field in ['tokens', 'exceptions']:
+        if field in rule_data:
+            markdown += f"**{field.capitalize()}:**\n"
+            for item in rule_data[field]:
+                markdown += f"- `{item}`\n"
+            markdown += "\n"
+
     if 'swap' in rule_data:
-        markdown += "\n**Swap:**\n"
+        markdown += "**Swap:**\n"
         if isinstance(rule_data['swap'], dict):
             for key, value in rule_data['swap'].items():
                 markdown += f"- `{key}` -> `{value}`\n"
         elif isinstance(rule_data['swap'], list):
             for item in rule_data['swap']:
                 markdown += f"- `{item}`\n"
+        markdown += "\n"
+
     if 'ignorecase' in rule_data:
-        markdown += f"\n**Ignore Case:** {rule_data['ignorecase']}\n"
-    markdown += "\n"
+        markdown += f"**Ignore Case:** {rule_data['ignorecase']}\n\n"
+
     return markdown
 
 def process_subfolder(subfolder_path):
     markdown_content = ""
-    for root, dirs, files in os.walk(subfolder_path):
+    for root, _, files in os.walk(subfolder_path):
         for file in files:
             if file.endswith('.yml'):
                 with open(os.path.join(root, file), 'r') as f:
@@ -47,7 +50,8 @@ def generate_table_of_contents(style_folders):
 
 def main():
     styles_folder = 'styles'
-    style_folders = [folder for folder in os.listdir(styles_folder) if os.path.isdir(os.path.join(styles_folder, folder))]
+    style_folders = [folder for folder in os.listdir(styles_folder)
+                     if os.path.isdir(os.path.join(styles_folder, folder))]
 
     markdown_content = "# Vale Linter Rules\n\n"
     markdown_content += "This page provides a comprehensive list of the Vale linter rules used in Datadog documentation.\n\n"
